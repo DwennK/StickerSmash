53d444d1ba6ddd7e5d1739bcc45b6d50eb56dd1d
DwennK <dwenn@live.fr>
2024-01-19 23:22:05 +0100

    add button.js

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image } from 'react-native';
 
+import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage}></ImageViewer>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -25,4 +43,8 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 58,
   },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  }
 });
